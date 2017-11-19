87c11ebc6241f61094a3562c0211deb3cf3b7a95
Michael Myung <michael.h.myung@gmail.com>
2017-11-19 05:36:39 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,23 @@
 'use strict';
+
+class Question {
+  constructor (content){
+  this.content = content;
+  this.save();
+  }
+
+  save(){
+    this.constructor._All.push(this);
+  }
+
+  static All(){
+    return Question._All;
+  }
+
+  static Find(id) {
+    return this.All()[id-1]
+  }
+
+}
+
+Question._All = []
